922013fd5347ac71b5d4e932a6c9794f88a15ebf
Turnip1234 <51388022+Turnip1234@users.noreply.github.com>
2020-03-15 13:05:38 -0700

    added text analyzer w/ brainjs

---
+++ essay.js
@@ -0,0 +1,20 @@
+const net = new brain.recurrent.LSTM();
+function submit() {
+var rawData = document.getElementById("input");
+var q = document.getElementById("q");
+var a = document.getElementById("answer");
+const data = rawData.value.toString().toLowerCase().split(". ");
+console.log("Training neural net...");
+a.style.display = "block";
+const d = new Date();
+net.train(data, {
+    iterations: parseInt(prompt("How much training? (More training takes longer)", "1000")),
+    log: true,
+    logPeriod: 100
+});
+console.log(`Neural net trained in ${(new Date() - d) /1000} seconds.`);
+q.style.display = "block";
+}
+function question() {
+    document.getElementById("answer").innerHTML = net.run(q.value.toString);
+}

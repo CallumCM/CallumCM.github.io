ce426707d1e7382f4a073662da6833f6395288d3
Turnip1234 <51388022+Turnip1234@users.noreply.github.com>
2020-03-18 09:25:36 -0700

    debugging worker.js cooperation with essay.js

--- essay.js
+++ essay.js
@@ -1,30 +1,61 @@
+var percent = document.getElementById("myBar");
 function submit() {
-    if(!window.Worker) {
-        const net = new brain.recurrent.LSTM();
-        var rawData = document.getElementById("input");
-        var q = document.getElementById("q");
-        var a = document.getElementById("answer");
-        const data = rawData.value.toString().toLowerCase().split(".").split(",").split("!").split("?");
-        console.log("Training neural net...");
-        a.style.display = "block";
-        const d = new Date();
-        net.train(data, {
-            iterations: parseInt(prompt("How much training? (More training takes longer)", "1000")),
-           log: true,
-           logPeriod: 100
-        });
-        console.log(`Neural net trained in ${(new Date() - d) /1000} seconds.`);
-        q.style.display = "block";
-    }
-    else {
-        var worker1 = new Worker('./worker.js');
-        worker1.postMessage();
-        worker1.onmessage = function(e) {
-            console.log(e.data)
+    var rawData = document.getElementById("input");
+    var q = document.getElementById("q");
+    var a = document.getElementById("answer");
+    const data = rawData.value.toString().toLowerCase().split(/[\.\,\?\!\;]/);
+    var maxIter = parseInt(prompt("How much training? (More training takes longer but provides higher quality results)", "1000"))
+    var increment = 100/maxIter;
+    viewBar(true);
+    console.log("Training neural net...");
+    const d = new Date();
+    if(window.Worker) {
+        var worker = new Worker('worker.js');
+        worker.postMessage([data, maxIter]);
+        worker.onmessage = function(e) {
+            if(e.data[1] == 0) {
+                progress(e.data)
+            } else if(e.data[1] == 1) {
+                const net = e.data;
+            } else {
+                var stats = e.data;
+            }
         }
     }
+    else {
+        console.warn("Because web workers not supported, program is running on single thread. You will get a huge lag spike.");
+        var stats = net.train(data, {
+        iterations: maxIter,
+        log: function() {
+            console.log();
+        },
+        logPeriod: 1,
+        learningRate: 0.6
+        });
+    }
+    viewBar(false)
+    console.log("iterations: " + stats.iterations.toString() + " training error: " + stats.error.toString());
+    console.log(`Neural net trained in ${(new Date() - d) /1000} seconds.`);
+    q.style.display = "block";
+    a.style.display = "block";
 }
 function question() {
     q = document.getElementById("q");
-    document.getElementById("answer").innerHTML = net.run(q.value.toString);
+    var ans = document.getElementById("answer");
+    ans.innerHTML = net.run(q.value.toString) + ".";
 }
+function progress(Bpercent=0) {
+    percent.style.width = Bpercent + "%";
+    percent.innerHTML = Bpercent + "%";
+    return Bpercent
+
+}
+function viewBar(bool) {
+    if(bool) {
+        document.querySelector("#loading").style.display = "block";
+    }
+    else {
+        document.querySelector("#loading").style.display = "none";
+    }
+}
+document.getElementById("input").onchange = question();
--- worker.js
+++ worker.js
@@ -1,1 +1,18 @@
-postMessage("hi");
+const net = new brain.recurrent.LSTM();
+onmessage = function(e) {
+    data = e.data[0];
+    maxIter = e.data[1];
+    increment = 100/maxIter;
+    p = 0;
+    var stats = net.train(data, {
+        iterations: maxIter,
+        log: function() {
+            p += increment;
+            postMessage([p, 0]);
+        },
+        logPeriod: 1,
+        learningRate: 0.6
+    });
+    postMessage([net, 1]);
+    postMessage([stats, 2]);
+}

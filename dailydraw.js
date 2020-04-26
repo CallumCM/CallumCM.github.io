2a6c3207fcd651dbc378a0540ced155b74a94b77
Turnip1234 <mauveshishito14@gmail.com>
2020-04-26 16:18:58 -0700

    added features to daily drawing challenge

--- dailydraw.js
+++ dailydraw.js
@@ -1,16 +1,33 @@
 function setup() {
     var canvas = createCanvas(710, 400);
+    var slider = document.getElementById("slider")
     canvas.parent('drawholder');
-    background(100);
-    strokeWeight(4);
+    canvas.id = 'editor'
+    background("#bbb");
+    document.getElementById('bcolor').value = "#010101";
+    strokeWeight(7);
+    slider.value = 7;
     stroke(color(document.getElementById("bcolor").value.toString()));
 }
 function draw() {
     if(mouseIsPressed) {
-    line(pmouseX, pmouseY, mouseX, mouseY);
+        line(pmouseX, pmouseY, mouseX, mouseY);
     }
     stroke(color(document.getElementById("bcolor").value.toString()));
 }
+slider.oninput = function() {
+    strokeWeight(slider.value);
+}
 function cleardraw() {
-    background(100);
+    background("#bbb");
+}
+function erasorPen() {
+    document.getElementById('bcolor').value = "#bbbbbb";
+    document.getElementById('bcolor').style.visibility = "hidden"; 
+    document.getElementById('hue').style.visibility = "hidden"; 
+    strokeWeight(32);
+}
+function penErasor() {
+    document.getElementById('bcolor').value = "#010101";
+    document.getElementById('bcolor').style.visibility = "visible"; 
 }
